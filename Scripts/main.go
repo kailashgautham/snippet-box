de620d61f9cfd6f3f18c59ef6a3c8ac018829497
kailashgautham <kailash.gautham@gmail.com>
2022-04-16 17:51:53 +0800

    Added git to PATH variable.

---
+++ Scripts/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+//this function writes a byte slice containing "Hello from SnippetBox" as the response body
+//this is the home handler function
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	//ResponseWriter is for assembling an HTTP response and sending to the user
+	//Request is a struct which holds info about the current request.
+
+	w.Write([]byte("Hello from SnippetBox"))
+
+}
+
+func main() {
+
+	//initialize a new servemux and register "home" as the handler for the "/" URL
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	//Use http.ListenAndServe() to start a new web server
+	//Two parameters: TCP network address to listen on, and the servemux
+	//If it returns an error, use log.Fatal() to record the error
+
+	log.Println("Starting server on :4000")  //The TCP address
+	err := http.ListenAndServe(":4000", mux) //Starting the web server
+	log.Fatal(err)                           //Log error if there is any
+
+}
