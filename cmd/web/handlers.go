0b7863d24890c26cf80f1f5df2f825a78c69678c
kailashgautham <kailash.gautham@gmail.com>
2022-04-17 10:38:38 +0800

    HTML files
    - Added template parsing for home page
    - Added HTML templates to be reused in other page files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from Snippetbox"))
+
+	//use template.ParseFiles() to read the template into a template set. If any error, log the error and use http.Error
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	//Execute() on template to write template as the response body. Last parameter is any dynamic data that we want to pass in
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 }
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
