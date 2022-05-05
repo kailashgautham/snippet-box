0f12d45a1f2382f2cc9aee5aa0c4b4630babd708
kailashgautham <kailash.gautham@gmail.com>
2022-05-05 12:25:48 +0800

    Cleaned up rest routing and added create snip page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,11 +10,6 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
-	if r.URL.Path != "/" && r.URL.Path != "/home" {
-		app.notFound(w)
-		return
-	}
-
 	//Execute() on template to write template as the response body. Last parameter is any dynamic data that we want to pass in
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -28,7 +23,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -46,12 +41,11 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.page.tmpl", nil)
+}
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
 	title := "0 snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n–Kobayashi Issa"
 	expires := "7"
@@ -62,7 +56,7 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id),
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id),
 		http.StatusSeeOther)
 
 }
