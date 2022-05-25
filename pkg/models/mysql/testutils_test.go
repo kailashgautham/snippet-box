a655efc26e33dd95ff56586c88fdea33b8b65d77
Kailash Gautham <kailash.gautham@gmail.com>
2022-05-25 23:05:37 +0800

    completion of testing

--- pkg/models/mysql/testutils_test.go
+++ pkg/models/mysql/testutils_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func newTestDB(t *testing.T) (*sql.DB, func()) {
-	db, err := sql.Open("mysql", "test_web:pass@/test_snippetbox?parsetime=true&multiStatements=true")
+	db, err := sql.Open("mysql", "test_web:pass@/test_snippetbox?parseTime=true&multiStatements=true")
 	if err != nil {
 		t.Fatal(err)
 	}
